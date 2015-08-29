fc14f479a129c16628ea9c6916277118f0b28f0d
MX2015 <501260519@qq.com>
2015-08-29 20:52:46 +0800

    Initial commit

---
+++ src/com/mx/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.mx.demo;
+
+/**
+ * Created by Administrator on 2015/8/29.
+ */
+public class Demo {
+
+   public static void main(String args []){
+       System.out.print("hello");
+   }
+}
